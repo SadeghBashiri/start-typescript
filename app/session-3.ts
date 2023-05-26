8a895c330d7b2ca9f966b6678b54a9fd5ab976f7
manred1987 <manred1987@gmail.com>
2023-05-26 12:16:30 +0330

    add session-3

---
+++ app/session-3.ts
@@ -0,0 +1,9 @@
+// [number, string, boolean, null, undefine, object, array]
+// [any, unknown, enum, tuple, never]
+// let فقط داخل اسکوپ در دسترس است
+// var کل فانکشن
+// const متغییری که تغییر نمیکند
+// typeof();
+let a;
+console.log(a); // undefined
+console.log(typeof(a));
