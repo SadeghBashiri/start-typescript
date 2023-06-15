e5a0d6f05c27dea655e2d700af87bfe1443bcfc6
manred1987 <manred1987@gmail.com>
2023-06-15 19:01:55 +0330

    Add session 13

---
+++ app/session-13-1.ts
@@ -0,0 +1,12 @@
+// import
+import { CarClass } from "./session-13-2";
+
+import { move, Car } from "./session-13-2";
+
+// use alias from duplicate
+import {Car as carInstance} from "./session-13-3"
+
+import { myCar } from "./session-13-2";
+
+// default import
+import anyAlias from './session-13-2'
